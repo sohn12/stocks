d03d57e3b10df0b671ec63121ed64abbba0cc86c
sohn12 <gantalasohanreddy@gmail.com>
2019-10-30 20:38:38 +0530

    final commit

---
+++ src/components/portfolio/Stock.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="col-sm-6 col-md-4">
+    <div class="panle panel-info">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          {{stock.name}}
+          <small>(Price: {{stock.price | currency}} | Quantity: {{stock.quantity}})</small>
+        </h3>
+      </div>
+      <div class="panel-body">
+        <div class="pull-left">
+          <input
+            type="number"
+            class="form-control"
+            :class="{danger:insufficientQuantity}"
+            placeholder="Quantity"
+            v-model="quantity"
+          />
+        </div>
+        <div class="pull-right">
+          <button
+            class="btn btn-success"
+            @click="sellStock"
+            :disabled="insufficientQuantity|| quantity <= 0 || !Number.isInteger(+quantity)"
+          >{{insufficientQuantity?'Insufficient':'Sell'}}</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  computed: {
+    insufficientQuantity() {
+      return this.quantity > this.stock.quantity;
+    }
+  },
+  methods: {
+    ...mapActions({
+      placeSellOrder: "sellStock"
+    }),
+    sellStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      };
+      this.placeSellOrder(order);
+      this.quantity = 0;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.danger {
+  border: 1px solid red;
+}
+</style>
